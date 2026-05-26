318764a03766d77bd081c2de8e8274b8f828d803
Bruno Ximenes <brunoxmns7@gmail.com>
2026-05-26 19:27:14 -0300

    feat: cria servico-saldo com endpoint /saldo

---
+++ backend/servico-saldo/src/main/java/com/itau/servico_saldo/ServicoSaldoApplication.java
@@ -0,0 +1,13 @@
+package com.itau.servico_saldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoSaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoSaldoApplication.class, args);
+	}
+
+}
---
+++ backend/servico-saldo/src/test/java/com/itau/servico_saldo/ServicoSaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.servico_saldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoSaldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
