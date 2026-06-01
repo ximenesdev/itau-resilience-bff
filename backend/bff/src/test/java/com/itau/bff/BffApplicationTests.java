2b2e4f017d058a738fb7a35836feb7b0fcde264c
Bruno Ximenes <brunoxmns7@gmail.com>
2026-06-01 14:49:03 -0300

    feat: adiciona BFF com Circuit Breaker

---
+++ backend/bff/src/test/java/com/itau/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
