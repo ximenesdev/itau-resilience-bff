a4b535c9013fc93342c434a1e94f410754186559
Bruno Ximenes <brunoxmns7@gmail.com>
2026-06-01 08:23:02 -0300

    feat: adiciona servico-cartao e servico-investimentos

---
+++ backend/servico-cartao/src/main/java/com/itau/servico_cartao/ServicoCartaoApplication.java
@@ -0,0 +1,13 @@
+package com.itau.servico_cartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCartaoApplication.class, args);
+	}
+
+}
---
+++ backend/servico-cartao/src/test/java/com/itau/servico_cartao/ServicoCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.servico_cartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
